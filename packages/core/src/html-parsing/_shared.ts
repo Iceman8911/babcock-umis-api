406b6f1470e85ca8d67f1c12f1a6d6e8c4602ecb
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-17 02:02:21 +0100

    test(core): add tests for node html parser

---
+++ packages/core/src/html-parsing/_shared.ts
@@ -0,0 +1,27 @@
+export const _SAMPLE_HTML_RESPONSE: Response = new Response(`<html lang="en">
+  <head>
+    <title>Example Domain</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <script type="text/javascript" src="chrome-extension://cmndjbecilbocjfkibfbifhngkdmjgog/core/scripts/inpage/sdk.script.js"></script>
+    <script src="chrome-extension://ecabpflfbpjfmccflbcjooejhiclhice/spoof-slow-network.js"></script>
+  </head>
+  <body>
+    <div>
+      <h1>Example Domain</h1>
+      <p>This domain is for use in documentation examples without needing permission. Avoid use in operations.</p>
+      <p><a href="https://iana.org/domains/example">Learn more</a></p>
+      <ul>
+        <li>First item</li>
+        <li>Second item</li>
+        <li>Third item</li>
+      </ul>
+      <form>
+        <label for="input1">Input:</label>
+        <input id="input1" type="text" placeholder="Type something..." />
+        <button type="submit">Submit</button>
+      </form>
+      <img src="https://via.placeholder.com/150" alt="Placeholder Image" />
+    </div>
+  </body>
+</html>
+`);
