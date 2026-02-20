18fc862697798b6163247c77b3566f5821628842
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-20 09:02:03 +0100

    wip(core): add umis page constant

---
+++ packages/core/src/constants/umis-pages.ts
@@ -0,0 +1,5 @@
+/** All the recored umiis page urls */
+export enum UmisPage {
+	SECURITY_CHECK = "https://umis.babcock.edu.ng/babcock/j_security_check",
+	DASHBOARD = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=1:0",
+}
