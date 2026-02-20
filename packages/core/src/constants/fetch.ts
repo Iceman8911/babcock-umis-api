b520bccb0f18580c41825cfe29aba7e6e1116f7a
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-20 11:22:48 +0100

    fix(core): improve shared fetch headers

--- packages/core/src/constants/fetch.ts
+++ packages/core/src/constants/fetch.ts
@@ -1,3 +1,16 @@
 export const SHARED_FETCH_HEADERS = {
+	accept: "*/*",
+	"accept-language": "en-US,en;q=0.9",
+	"content-type": "application/x-www-form-urlencoded",
+	cookie: "JSESSIONID=F4E587B11030766AA20F9ABAF3B9B7C4",
+	Host: "umis.babcock.edu.ng",
 	Origin: "https://umis.babcock.edu.ng",
+	"save-data": "on",
+	"sec-ch-ua":
+		'"Not(A:Brand";v="8", "Chromium";v="144", "Microsoft Edge";v="144"',
+	"sec-ch-ua-mobile": "?0",
+	"sec-ch-ua-platform": '"Linux"',
+	"sec-fetch-dest": "empty",
+	"sec-fetch-mode": "cors",
+	"sec-fetch-site": "same-origin",
 } as const;
