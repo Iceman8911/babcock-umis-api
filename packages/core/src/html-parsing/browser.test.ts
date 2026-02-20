14787a26084090b99c5c36f1906d5cc07fc04413
Iceman8911 <wuchiJSS3@gmail.com>
2026-02-20 09:53:00 +0100

    test(core): unregister happy dom after use
    otherwise, it'd conflict with some tests

--- packages/core/src/html-parsing/browser.test.ts
+++ packages/core/src/html-parsing/browser.test.ts
@@ -1,3 +1,4 @@
+import { afterAll } from "bun:test";
 import { GlobalRegistrator } from "@happy-dom/global-registrator";
 import { runHTMLParserSharedTests } from "./_html-parser.shared-test";
 
@@ -9,3 +10,5 @@ const { default: BrowserHTMLParser } = await import("./browser");
 runHTMLParserSharedTests(BrowserHTMLParser.name, (htmlRes) =>
 	BrowserHTMLParser.init(htmlRes),
 );
+
+afterAll(() => GlobalRegistrator.unregister());
