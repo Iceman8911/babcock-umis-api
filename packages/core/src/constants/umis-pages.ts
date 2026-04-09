c026705c573c2e99da484dd56c6604c4d25b6b05
Iceman8911 <wuchiJSS3@gmail.com>
2026-04-09 16:20:53 +0100

    feat(core): complete umis links

--- packages/core/src/constants/umis-pages.ts
+++ packages/core/src/constants/umis-pages.ts
@@ -3,4 +3,34 @@ export enum UmisPage {
 	SECURITY_CHECK = "https://umis.babcock.edu.ng/babcock/j_security_check",
 	CHECK_USER = "https://umis.babcock.edu.ng/babcock/user_checks",
 	DASHBOARD = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=1:0",
+	SCHOOL_INFO = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=10:0",
+	PERSONAL_DETAILS = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=112:0",
+
+	// Academic details
+	SEMESTER_RESULTS = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=19:0",
+	UNOFFICIAL_TRANSCRIPT = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=21:0",
+	REPEATED_COURSES = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=91:0",
+	CHECK_LISTING = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=26:0",
+	POST_QUERY = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=32:0",
+
+	// Registration
+	COMMENCE_REGISTRATION = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=22:0",
+	SELECT_COURSES = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=24:0",
+	SELECT_MEAL_TYPE = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=100:0",
+	SELECT_RESIDENCE = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=23:0",
+	WORSHIP_CENTER = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=49:0",
+	SELECTED_COURSE_LIST = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=27:0",
+	SELECTED_TIMETABLE = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=28:0",
+	OUTSTANDING_COURSES = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=103:0",
+	SUBMIT_REGISTRATION = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=29:0",
+
+	// Finance
+	FINANCE_STATEMENT = "https://umis.babcock.edu.ng/babcock/a_statement.jsp?view=33:0",
+	SELECT_PAYMENTS = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=102:0",
+	CURRENT_CHARGES = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=33:0",
+	CHECK_FINANCE_STATUS = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=39:0",
+	CHECK_REGISTRATION_STATUS = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=30:0",
+	FINANCE_CLEARANCE = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=40:0",
+	PRINT_COURSE_FORM = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=50:0",
+	PAY_FOR_PREVIOUS_SEMESTER = "https://umis.babcock.edu.ng/babcock/a_students.jsp?view=44:0",
 }
