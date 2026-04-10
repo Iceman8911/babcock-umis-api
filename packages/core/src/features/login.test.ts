9b4fcb355cbd9961c87974bd93d9c39410292d48
Iceman8911 <wuchiJSS3@gmail.com>
2026-04-10 12:21:44 +0100

    feat(core): propagate Result through login flow

--- packages/core/src/features/login.test.ts
+++ packages/core/src/features/login.test.ts
@@ -17,12 +17,20 @@ describe(attemptStudentLogin.name, () => {
 	it("should successfully log in an existing user", async () => {
 		const result = await attemptStudentLogin(CORRECT_LOGIN_PAYLOAD);
 
-		expect(result).not.toBeNull();
+		expect(result.success).toBe(true);
+
+		if (!result.success) throw Error(`Login failed with error: ${result.err}`);
+
+		expect(result.val).toBeDefined();
 	});
 
 	it("should fail to log in a non-existing user", async () => {
 		const result = await attemptStudentLogin(WRONG_LOGIN_PAYLOAD);
 
-		expect(result).toBeNull();
+		expect(result.success).toBe(false);
+
+		if (result.success) throw Error("Login should have failed but succeeded");
+
+		expect(result.err).toBeDefined();
 	});
 });
--- packages/core/src/features/login.ts
+++ packages/core/src/features/login.ts
@@ -9,30 +9,43 @@ import {
 	type StudentCredentialsInput,
 	StudentCredentialsSchema,
 } from "../models/credentials";
+import type { Result } from "../models/result.types";
+import { getErrorMessage } from "../utils/error";
 import { doesStudentExist } from "./check-user";
 
 /** Returns the cookies if the user is logged in, else `null` */
 export async function attemptStudentLogin(
 	credentials: Readonly<StudentCredentialsInput>,
-): Promise<UserLoginCookieOutput | null> {
-	const parsedCredentials = v.parse(StudentCredentialsSchema, credentials);
+): Promise<Result<UserLoginCookieOutput, string>> {
+	try {
+		const parsedCredentials = v.parse(StudentCredentialsSchema, credentials);
 
-	if (!(await doesStudentExist(credentials.user))) return null;
+		const studentExistenceResult = await doesStudentExist(
+			parsedCredentials.j_username,
+		);
 
-	const request = new Request(UmisPage.SecurityCheck, {
-		body: new URLSearchParams(parsedCredentials),
-		credentials: "include",
-		headers: {
-			...SHARED_FETCH_HEADERS,
-			"Content-Type": "application/x-www-form-urlencoded",
-			Referer: UmisPage.Dashboard,
-		},
-		method: "POST",
-	});
+		if (!studentExistenceResult.success) return studentExistenceResult;
 
-	const res = await fetch(request);
+		const request = new Request(UmisPage.SecurityCheck, {
+			body: new URLSearchParams(parsedCredentials),
+			credentials: "include",
+			headers: {
+				...SHARED_FETCH_HEADERS,
+				"Content-Type": "application/x-www-form-urlencoded",
+				Referer: UmisPage.Dashboard,
+			},
+			method: "POST",
+		});
 
-	return res.ok
-		? v.parse(UserLoginCookieSchema, res.headers.get("set-cookie"))
-		: null;
+		const res = await fetch(request);
+
+		return res.ok
+			? {
+					success: true,
+					val: v.parse(UserLoginCookieSchema, res.headers.get("set-cookie")),
+				}
+			: { err: "Login failed", success: false };
+	} catch (e) {
+		return { err: getErrorMessage(e), success: false };
+	}
 }
