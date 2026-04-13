bfc21daf8802a5779ece06c46e077687dbf8a49a
Iceman8911 <wuchiJSS3@gmail.com>
2026-04-13 22:13:26 +0100

    refactor(core, shared): remove unused type export

--- packages/core/src/features/shared/university-level.ts
+++ packages/core/src/features/shared/university-level.ts
@@ -5,7 +5,6 @@ export const UniversityLevelSchema = v.pipe(
 	ParseIntegerSchema,
 	v.picklist([100, 200, 300, 400, 500, 600]),
 );
-export type UniversityLevelInput = v.InferInput<typeof UniversityLevelSchema>;
 export type UniversityLevelOutput = v.InferOutput<typeof UniversityLevelSchema>;
 
 export const UniversityYearSchema = v.fallback(
